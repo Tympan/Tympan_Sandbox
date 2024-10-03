1982fe1aa95191c37a8083329ec684b74218a66d
Chip Audette <chipaudette@yahoo.com>
2024-10-03 12:31:44 -0400

    Bluetooth: nRF52840 firmware: remove (and move to Tympan Rev F Hardware repo)

--- Tympan Bluetooth/nRF52840 Firmware/nRF52840_firmware/chip_program_full.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\Nordic Semiconductor\nrf-command-line-tools\bin\nrfjprog.exe" --program "C:\Users\Chip\AppData\Local\Arduino15\packages\adafruit\hardware\nrf52\1.6.1\bootloader\feather_nrf52840_express\feather_nrf52840_express_bootloader-0.9.0_s140_6.1.1.hex" --family NRF52 --chiperase --reset --verify && nrfjprog --memwr 0xFF000 --val 0x01 && nrfjprog --reset --program build\adafruit.nrf52.feather52840\nRF52840_firmware.ino.hex --sectorerase --family NRF52 --verify
-
